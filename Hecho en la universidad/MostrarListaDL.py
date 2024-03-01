102835b84e3ae0b995b3a4e4b6a09e6335c9ff1e
AdrianALM766 <adrianantoniolopezmonterrosa@gmail.com>
2024-03-01 18:27:28 -0500

    avances 01/03/2024

--- Hecho en la universidad/MostrarListaDL.py
+++ Hecho en la universidad/MostrarListaDL.py
@@ -40,11 +40,28 @@ class ListaDoblementeLigada:
         print("")
         print("")
         print("")
-
+    def menu (self):
+        me=True
+        while (me==True):
+            print("MENU")
+            print("Crear lista doblemente ligada=1")
+            print("mostrar de adelante hacia atras=2")
+            print("promedio de atras hacia adelante=3")
+            print("salir=4")
+            opc=int(input("Ingrese la opcion: "))
+            print("-"*10)
+            print("-"*10)
+            if (opc==1):
+                dato=int(input("digame el dato: "))
+                self.ins_lista(dato)
+            elif(opc==2):
+                self.mostrarListaAdelante()
+            elif(opc==3):
+                self.mostrarListaAtras()
+            elif(opc==4):
+                print("listo")
+                me=False
+            else:
+                print("elija otra opcion.")
 l=ListaDoblementeLigada()
-aux=5
-a=6
-l.ins_lista(aux)
-l.ins_lista(a)
-l.mostrarListaAdelante()
-l.mostrarListaAtras()
+l.menu()
