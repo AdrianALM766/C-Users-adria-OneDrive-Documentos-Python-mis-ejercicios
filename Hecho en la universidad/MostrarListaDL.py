6742759cc0e1dbb04512da0cb935a958dce33392
AdrianALM766 <adrianantoniolopezmonterrosa@gmail.com>
2024-03-02 15:33:54 -0500

    avances 02/03/2024

--- Hecho en la universidad/MostrarListaDL.py
+++ Hecho en la universidad/MostrarListaDL.py
@@ -1,67 +1,86 @@
+class Color:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    RESET = '\033[0m'
+
 class Nodo:
     def __init__(self,dato):
         self.dato=dato
-        self.ld = None
+        #anterior
         self.li = None
+        #siguiente
+        self.ld = None
         
 class ListaDoblementeLigada:
     def __init__(self):
         self.pri = None
         self.fin=None
 
-    def ins_lista(self,dato):
+    def ins_alfinal(self,dato):
         nodo=Nodo(dato)
-        if(self.pri== None):
+        if self.pri== None:
             self.pri=nodo
             self.fin=nodo
         else:
-            nodo.ld= self.pri
-            self.pri.li=nodo
-            self.pri=nodo
-    def mostrarListaAdelante(self):
-        actual=self.pri
-        print("")
-        print("")
-        print("")
-        while actual != None:
+            self.fin.ld= nodo
+            nodo.li=self.fin
+            self.fin=nodo
+
+    def mostrarListaAdelante(self,actual):
+        if actual == None:
+            print("")
+        else:
+            print(actual.dato, end=" -> ")
+            self.mostrarListaAdelante(actual.ld)
+
+    def mostrarListaAtras(self,actual):
+        if actual==None:
+            print("")
+        else:
             print(actual.dato,end= " -> ")
-            actual=actual.ld
-        print("")
-        print("")
-        print("")
-    def mostrarListaAtras(self):
-        actual=self.fin
-        print("")
-        print("")
-        print("")
-        while actual != None:
-            print(actual.dato,end= " -> ")
-            actual=actual.li
-        print("")
-        print("")
-        print("")
-    def menu (self):
-        me=True
-        while (me==True):
-            print("MENU")
-            print("Crear lista doblemente ligada=1")
-            print("mostrar de adelante hacia atras=2")
-            print("promedio de atras hacia adelante=3")
-            print("salir=4")
-            opc=int(input("Ingrese la opcion: "))
-            print("-"*10)
-            print("-"*10)
-            if (opc==1):
-                dato=int(input("digame el dato: "))
-                self.ins_lista(dato)
-            elif(opc==2):
-                self.mostrarListaAdelante()
-            elif(opc==3):
-                self.mostrarListaAtras()
-            elif(opc==4):
-                print("listo")
-                me=False
+            self.mostrarListaAtras(actual.li)
+    def menu(self):
+        me = True
+        while me:
+            print(Color.GREEN)
+            print( "|" + "=" * 54 + "|")
+            print("MENU".center(56))  # Centra el título en una línea de 56 caracteres
+            print("1. Instanciar lista".center(56))
+            print("2. Mostrar lista de adelante hacia atrás".center(56))
+            print("3. Mostrar lista de atrás hacia adelante".center(56))
+            print("4. Salir".center(56))
+            print("|" + "=" * 54 + "|")
+            print(Color.MAGENTA)
+            opc = int(input("Ingrese la opción: ".center(50)))
+            print(Color.RESET)
+            if opc == 1:
+                print(Color.YELLOW)
+                dato = int(input("Ingrese dato: ".center(50)))
+                print(Color.RESET)
+                self.ins_alfinal(dato)
+            elif opc == 2:
+                print(Color.MAGENTA)
+                aux = self.pri
+                self.mostrarListaAdelante(aux)
+                print(Color.RESET)
+            elif opc == 3:
+                print(Color.MAGENTA)
+                aux = self.fin
+                self.mostrarListaAtras(aux)
+                print(Color.RESET)
+            elif opc == 4:
+                print(Color.RED)
+                print("Saliendo del programa...".center(56))
+                print(Color.RESET)
+                me = False
             else:
-                print("elija otra opcion.")
+                print(Color.RED)
+                print("Elija otra opción.".center(56))
+                print(Color.RESET)
+
 l=ListaDoblementeLigada()
 l.menu()
--- Hecho en la universidad/SumarVectorRecursividad.py
+++ Hecho en la universidad/SumarVectorRecursividad.py
@@ -1,34 +1,47 @@
-def suma_recursiva(vector, indice=0):
-    if indice >=len(vector):
-        return 0
-    else:
-        return vector[indice] + suma_recursiva(vector, indice + 1)
+class Color:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    RESET = '\033[0m'
 
-
-
+def suma_recursiva(vector, indice=1):
+        if indice >=len(vector):
+            return 0
+        else:
+            return vector[indice] + suma_recursiva(vector, indice + 1)
 
 def menu():
-    vector=[]
-    me=True
-    while (me==True):
-            print("MENU")
-            print("Crear vector=1")
-            print("mostrar suma=2")
-            print("salir=3")
-            opc=int(input("Ingrese la opcion: "))
-            print("-"*10)
-            print("-"*10)
-            if (opc==1):
-                dato=int(input("ingrese el dato: "))
-                vector+=[dato]
-                print(vector)
-            elif(opc==2):
-                resul=suma_recursiva(vector)
-                print("la suma recursiva es: ",resul)
-            elif(opc==3):
-                print("listo")
-                me=False
+    vector = [None]
+    me = True
+    while me:
+        print(Color.GREEN)
+        print("|" + "=" * 54 + "|")
+        print("MENU".center(56))
+        print("1. Crear vector".center(56))
+        print("2. Mostrar suma".center(56))
+        print("3. Salir".center(56))
+        print("|" + "=" * 54 + "|")
+        print(Color.RESET)
+        opc = int(input(Color.YELLOW + "Ingrese la opción: ".center(56) + Color.RESET))
+        if opc == 1:
+            dato = int(input(Color.YELLOW + "Ingrese el dato: ".center(56) + Color.RESET))
+            vector.append(dato)
+        elif opc == 2:
+            if len(vector) > 1:
+                resul = suma_recursiva(vector)
+                print(Color.MAGENTA)
+                print("La suma recursiva es: {}".format(resul).center(56))
+                print(Color.RESET)
             else:
-                print("elija otra opcion.")
+                print(Color.RED + "No hay elementos en el vector." + Color.RESET)
+        elif opc == 3:
+            print(Color.RED + "Saliendo del programa...".center(56) + Color.RESET)
+            me = False
+        else:
+            print(Color.RED + "Opción inválida. Por favor, elija otra opción." + Color.RESET)
+
 
 menu()
