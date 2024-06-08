44aa03777dc9f29f87ef61f39a9e8a1c8fa8c275
AdrianALM766 <adrianantoniolopezmonterrosa@gmail.com>
2024-06-08 17:46:33 -0500

    calendario 08/06/2024

--- Hecho en mi tiempo libre/calendario.py
+++ Hecho en mi tiempo libre/calendario.py
@@ -1,5 +1,15 @@
 #imprimir un año y mes especifico
 import calendar
-año= int(input("Escriba el año: "))
-mes= int(input("escriba el mes: "))
-print(calendar.month(año,mes))
+
+
+def Menu():
+    print()
+    year=int(input("Porfavor digite el numero del año: "))
+    month=int(input("Porfavor digite el mes: "))
+    while month>12 or month<1:
+        if month>12 or month<1:
+            print("Porfavor digite un mes que exista: ")
+            month=int(input())
+    print(calendar.month(year,month))
+
+Menu()
